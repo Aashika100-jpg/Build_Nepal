72a1f44eb066c02e18d3a9c373a4734a0d983905
Aashika100-jpg <aashika100pandey@gmail.com>
2026-08-01 17:08:19 +0545

    complain1e

--- lib/screens/complaint_screen.dart
+++ lib/screens/complaint_screen.dart
@@ -11,3 +11,21 @@ class ComplaintScreen extends StatefulWidget {
   @override
   State<ComplaintScreen> createState() => _ComplaintScreenState();
 }
+
+class _ComplaintScreenState extends State<ComplaintScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _vendorNameController = TextEditingController();
+
+  String? _selectedCategory;
+  String? _base64Image;
+  bool _isLoading = false;
+
+  // Added Icons to categories for a richer UI experience
+  final List<Map<String, dynamic>> _categories = [
+    {
+      'name': 'Overcharged',
+      'icon': Icons.attach_money_rounded,
+      'color': Colors.orange,
+    },
+    {
