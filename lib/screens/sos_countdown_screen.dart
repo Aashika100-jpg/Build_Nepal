619d487aa7c21e708a95f0b6d6a80bd83f75cde7
Aashika100-jpg <aashika100pandey@gmail.com>
2026-08-01 18:49:20 +0545

    countdwn

--- lib/screens/sos_countdown_screen.dart
+++ lib/screens/sos_countdown_screen.dart
@@ -5,3 +5,49 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:url_launcher/url_launcher.dart'; // NEW IMPORT
 import 'emergency_active_screen.dart';
+
+class SOSCountdownScreen extends StatefulWidget {
+  const SOSCountdownScreen({super.key});
+
+  @override
+  State<SOSCountdownScreen> createState() => _SOSCountdownScreenState();
+}
+
+class _SOSCountdownScreenState extends State<SOSCountdownScreen>
+    with SingleTickerProviderStateMixin {
+  int _timeLeft = 5;
+  Timer? _timer;
+  bool _isSending = false;
+  late AnimationController _pulseController;
+  late Animation<double> _pulseAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Setup pulsing animation for urgency
+    _pulseController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    )..repeat(reverse: true);
+
+    _pulseAnimation = Tween<double>(begin: 1.0, end: 1.2).animate(
+      CurvedAnimation(parent: _pulseController, curve: Curves.easeInOut),
+    );
+
+    _startCountdown();
+  }
+
+  void _startCountdown() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      HapticFeedback.heavyImpact(); // Vibrate phone on each tick
+      if (_timeLeft > 1) {
+        setState(() {
+          _timeLeft--;
+        });
+      } else {
+        _timer?.cancel();
+        _triggerEmergency();
+      }
+    });
+  }
